08f58e7065c542e9af10fefd5f7c26b3e657753c
AmirParsa <amirparsa.shk@gmail.com>
2026-07-08 21:29:16 +0330

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        maven { url = uri("https://maven.myket.ir") }
+        maven { url = uri("https://jitpack.io") }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        google()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.myket.ir") }
+    }
+}
+
+rootProject.name = "HesabYar"
+include(":app")
